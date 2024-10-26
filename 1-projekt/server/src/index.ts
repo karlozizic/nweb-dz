4c4842ba27b7b02fdf4ac5ef5be785b67a8943f3
kzizic <karlo.zizic1@gmail.com>
2024-10-26 10:30:47 +0200

    index.ts

--- 1-projekt/server/src/index.ts
+++ 1-projekt/server/src/index.ts
@@ -1,10 +1,30 @@
 import express, { Request, Response } from 'express';
+import https from 'https';
+import fs from 'fs';
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-const externalUrl = process.env.RENDER_EXTERNAL_URL || `http://localhost:${port}`;
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
 
-app.listen(<number>port, '0.0.0.0', () => {
-    console.log(`Server run on ${externalUrl}`);
-});
+const config = {
+    baseURL: externalUrl || `https://localhost:${port}`,
+};
+
+if (externalUrl) {
+    const hostname = '0.0.0.0';
+    app.listen(port, hostname, () => {
+        console.log(`locally run on: http://${hostname}:${port}/ available on url: ${externalUrl}`);
+    });
+}
+else {
+    https.createServer(
+        {
+            key: fs.readFileSync('server.key'),
+            cert: fs.readFileSync('server.cert'),
+        },
+        app
+    ).listen(port, () => {
+        console.log(`server run on: https://localhost:${port}/`);
+    });
+}
